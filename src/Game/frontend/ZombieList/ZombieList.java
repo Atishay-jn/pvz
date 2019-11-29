23b52bce5120c8b7fd6c494a6581fd708e544e47
Atishay <Atishay18026@iiitd.ac.in>
2019-11-29 17:24:09 +0530

    Zombie List for Almanac

--- src/Game/frontend/ZombieList/ZombieList.java
+++ src/Game/frontend/ZombieList/ZombieList.java
@@ -45,37 +45,37 @@ public class ZombieList
 			case "Lawn Zombie":
 				zombieImage.setImage(new Image("./Game/assets/backend/Zombies/LawnZombie.gif"));
 				zombieImage.setVisible(true);
-				zombieDescription.setText("Lawn Zombie");
+				zombieDescription.setText("Lawn Zombie. The generic friendly zombie looking for your brains.");
 				zombieDescription.setVisible(true);
 				break;
 			case "Traffic Zombie":
 				zombieImage.setImage(new Image("./Game/assets/backend/Zombies/TrafficZombie.gif"));
 				zombieImage.setVisible(true);
-				zombieDescription.setText("Traffic Zombie");
+				zombieDescription.setText("Traffic Zombie. This one crossed a road to get to your home. Please let him through");
 				zombieDescription.setVisible(true);
 				break;
 			case "Bucket Zombie":
 				zombieImage.setImage(new Image("./Game/assets/backend/Zombies/BucketZommbie.gif "));
 				zombieImage.setVisible(true);
-				zombieDescription.setText("Bucket Zombie");
+				zombieDescription.setText("Bucket Zombie. Ran out of water? Can I please finish bathing");
 				zombieDescription.setVisible(true);
 				break;
 			case "Rugby Zombie":
 				zombieImage.setImage(new Image("./Game/assets/backend/Zombies/RugbyZombie.png"));
 				zombieImage.setVisible(true);
-				zombieDescription.setText("Rugby Zombie");
+				zombieDescription.setText("Rugby Zombie. This zombie has played in the NFL, the best quarterback of all. Can take a hit or more ");
 				zombieDescription.setVisible(true);
 				break;
 			case "Runner Zombie":
 				zombieImage.setImage(new Image("./Game/assets/backend/Zombies/RunnerZombie.gif "));
 				zombieImage.setVisible(true);
-				zombieDescription.setText("Runner Zombie");
+				zombieDescription.setText("Runner Zombie. This zombie will roll his through your yard before you even realize");
 				zombieDescription.setVisible(true);
 				break;
 			case "Boss Zombie":
 				zombieImage.setImage(new Image("./Game/assets/backend/Zombies/BossZombie.gif "));
 				zombieImage.setVisible(true);
-				zombieDescription.setText("Boss Zombie");
+				zombieDescription.setText("Boss Zombie. The name is enough.");
 				zombieDescription.setVisible(true);
 				break;
 			default:

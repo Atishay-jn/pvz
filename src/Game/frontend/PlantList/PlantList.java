82e4d7485b7f6f6de6cf98508f8077c87301b219
Atishay <Atishay18026@iiitd.ac.in>
2019-11-29 18:13:32 +0530

    Plant List font

--- src/Game/frontend/PlantList/PlantList.java
+++ src/Game/frontend/PlantList/PlantList.java
@@ -34,7 +34,7 @@ public class PlantList
 
 	private static void setUpChoices()
 	{
-		plantChoice.getItems().addAll("Pea Shooter", "---", "Wallnut", "Tallnut", "Firey", "Frosty", "Gun", "Repeater", "Jalapeno", "Potato Mine", "Sunflower", "Twin Sunflower");
+		plantChoice.getItems().addAll( "---", "Pea Shooter", "Wallnut", "Tallnut", "Firey", "Frosty", "Gun", "Repeater", "Jalapeno", "Potato Mine", "Sunflower", "Twin Sunflower");
 		plantChoice.setValue("---");
 		plantChoice.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> newChoice(newValue));
 	}
@@ -45,13 +45,13 @@ public class PlantList
 			case "Wallnut":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Barrier/Wallnut/Wallnut.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("A small barrier. Won't let anyone through while its alive");
+				plantDescription.setText("A small barrier. \nWon't let anyone through while its alive");
 				plantDescription.setVisible(true);
 				break;
 			case "Tallnut":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Barrier/Tallnut/Tallnut.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("Big brother of Wallnut with life's motto: You shall not pass");
+				plantDescription.setText("Big brother of Wallnut \nLife's motto: You shall not pass");
 				plantDescription.setVisible(true);
 				break;
 			case "Pea Shooter":
@@ -63,49 +63,49 @@ public class PlantList
 			case "Firey":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Shooters/Firey/Firey.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("Pea shooter with a fire inside. Fire peas cause twice the damage and unfreeze any frozen zombies");
+				plantDescription.setText("Pea shooter with a fire inside. \nFire peas cause twice the damage and\n unfreeze any frozen zombies");
 				plantDescription.setVisible(true);
 				break;
 			case "Frosty":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Shooters/Frosty/Frosty.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("Pea shooter for the cold. Slows down all zombies hit by it by 0.5x");
+				plantDescription.setText("Pea shooter for the cold. \nSlows down all zombies hit by it by 0.5x");
 				plantDescription.setVisible(true);
 				break;
 			case "Gun":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Shooters/Gun/Gun.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("The repeater got even faster, 4 peas in the same time pea shooter shoots one");
+				plantDescription.setText("The repeater got even faster!\n4 peas in the same time pea shooter \nshoots one");
 				plantDescription.setVisible(true);
 				break;
 			case "Repeater":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Shooters/Repeater/Repeater.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("The pea shooter just got faster. Shoots twice the number of peas");
+				plantDescription.setText("The pea shooter just got faster. \nShoots twice the number of peas");
 				plantDescription.setVisible(true);
 				break;
 			case "Jalapeno":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Bomb/Jalapeno/Jalapeno.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("Is that smoke? Jalapeno burns the entire row at once sparing none");
+				plantDescription.setText("Is that smoke? \nJalapeno burns the entire row at once \nsparing none");
 				plantDescription.setVisible(true);
 				break;
 			case "Potato Mine":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Bomb/PotatoMine/PotatoMineArmed.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("Yikes! Potato mine is the perfect old school mine. Come too close and that's the last time you do. But remember it takes a while to activate");
+				plantDescription.setText("Yikes! \nPotato mine is the perfect old school \nmine. Come too close and that's the last \ntime you do. But remember it takes \na while to activate");
 				plantDescription.setVisible(true);
 				break;
 			case "Sunflower":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Collector/Sunflower/Sunflower.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("The perfect source continuous stream of inputs. Gives you extra sun tokens");
+				plantDescription.setText("The perfect source continuous stream\nof Sun. Gives you extra sun tokens");
 				plantDescription.setVisible(true);
 				break;
 			case "Twin Sunflower":
 				plantImage.setImage(new Image("./Game/assets/backend/Plants/Collector/TwinSunflower/TwinSunflower.gif"));
 				plantImage.setVisible(true);
-				plantDescription.setText("Sunflower on steroids. It's sun tokens are worth twice that of the normal Sunflower. ");
+				plantDescription.setText("Sunflower on steroids. \nIt's sun tokens are worth twice that of the normal Sunflower.");
 				plantDescription.setVisible(true);
 				break;
 			default:

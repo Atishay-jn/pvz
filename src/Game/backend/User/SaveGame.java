d91b4b7d10a507dd6e72e588d1eb128a9a35c4f6
Atishay <Atishay18026@iiitd.ac.in>
2019-11-30 11:46:33 +0530

    Game saved

--- src/Game/backend/User/SaveGame.java
+++ src/Game/backend/User/SaveGame.java
@@ -1,9 +1,18 @@
 package Game.backend.User;
 
+import java.io.*;
+
 public abstract class SaveGame extends CurrentUser
 {
-	public static void serialize()
-	{
-
+	public static void serialize() throws IOException {
+		ObjectOutputStream out = null;
+		try {
+			out = new ObjectOutputStream(new FileOutputStream("UserFiles/"+user.getName()+"/"+"userdata.text"));
+			out.writeObject(user);
+		}
+		finally
+		{
+			out.close();
+		}
 	}
 }
